75cfaf69b3a68b163ef12670e353504e6f3c0d13
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-05 07:08:28 +0000

    Create routes for each component and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import PageNotFound from "./pages/PageNotFound"
+import ProtectedRoute from "./ui/ProtectedRoute"
+import AppLayout from "./ui/AppLayout"
+import Dashboard from "./pages/Dashboard"
+import Courses from "./pages/Courses"
+import Reports from "./pages/Reports"
+import Course from "./pages/Course"
+import Report from "./pages/Report"
 
 
 function App() {
-  const x = 23
 
   return (
-   <div className="">Hello world</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route
+        element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Navigate replace to="dashboard" />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="courses" element={<Courses />} />
+        <Route path="courses/:courseId" element={<Course />} />
+        <Route path="reports" element={<Reports />} />
+      </Route>
+
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>
+    </>
   )
 }
 
