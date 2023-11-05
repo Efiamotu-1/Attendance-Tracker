567b2773f1a1943fdbf78f2af33a95fe675542f6
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-05 07:08:11 +0000

    Add tailwind configurations where necessary

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 2 column grid
+        'dash': '16.5rem, 1fr'
+      },
+      gridTemplateRows: {
+        'dash': 'auto 1fr'
+      },
+      gridTemplateColumns: {
+        // Simple 1 column grid
+        'dash-sm': '1fr'
+      },
+    },
   },
   plugins: [],
 }
