9d90394902e37e3576b006fed29fc03b15411bd5
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-05 07:13:16 +0000

    Add home dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <p>
+        Welcome to the Attendance tracker application. To begin you would have to create a course or courses depending 
+        on the number of courses you are taking for the semester, you can do this by accessing the course tab on the menu. 
+        After creating a course you can add daily reports using the reports tab based on whether the class held and whether 
+        you attended the class. Please note that these reports are not editable once added so make sure to only create a report 
+        when you have all the necessary information aabout a particular class. Alternatively you can edit and delete a course. 
+      </p>
+    </div>
+  )
+}
+
+export default Dashboard
