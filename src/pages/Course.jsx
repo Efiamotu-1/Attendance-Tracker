e8f07fd7628c95f6698cb99515d79bb986a39585
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-05 07:12:51 +0000

    Add page to display a single course

---
+++ src/pages/Course.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CourseDetail from '../features/courses/CourseDetail'
+
+function Course() {
+  return (
+    <div>
+      <CourseDetail />
+    </div>
+  )
+}
+
+export default Course
