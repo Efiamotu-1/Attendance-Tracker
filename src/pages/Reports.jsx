40434e96483f12b626690cd369f3703719f72a4e
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-07 01:14:45 +0000

    add reports and sign up pages

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReportTable from '../features/reports/ReportTable'
 import AddReport from '../features/reports/AddReport'
+import { useCourses } from '../features/courses/useCourses'
+import Spinner from '../ui/Spinner'
 
 function Reports() {
+  const {courses, isLoading} = useCourses()
+  if(isLoading) return <Spinner />
   return (
     <div className='flex flex-col gap-5'>
     <div>
       <h4>All Reports</h4>  
     </div>
-    <ReportTable />
-    <AddReport />
+    <ReportTable courses={courses}/>
+    {courses.length > 0 && <AddReport courses={courses}/>
+}
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import SignupForm from "../features/authentication/SignupForm";
 
 function Signup() {
+  const navigate = useNavigate()
   return (
     <div className="bg-[#111827] h-screen grid content-center justify-center gap-2 text-white">
       <h4 className="text-center font-semibold leading-normal text-[1.5rem]">Create a new account</h4>
       <SignupForm />
+      <p className="text-center mt-2">Have an Account <span 
+      onClick={() => navigate('/login')}
+      className="underline ml-1 text-blue-300 font-semibold text-[1rem] cursor-pointer">LOGIN </span></p>
     </div>
   );
 }
