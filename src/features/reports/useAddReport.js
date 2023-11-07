4a8695624a484756ed00b5b3eea7117ebc00c778
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-07 01:13:37 +0000

    create hook to handle adding a new report

---
+++ src/features/reports/useAddReport.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addReport as addReportApi} from "../../services/apiReports";
+import toast from "react-hot-toast";
+
+
+export function useAddReport() {
+    const queryClient = useQueryClient()
+    const {mutate: addReport, isLoading: isAdding} = useMutation({
+    mutationFn: ({courseId, classDate, held, attended}) => addReportApi({courseId, classDate, held, attended}),
+    onSuccess: () => {
+        toast.success("Report Added Successfully")
+        queryClient.invalidateQueries({ queryKey: ["reports"] })
+    },
+    onError: (err) => {
+        toast.error(err.message)
+    }
+})
+return {addReport, isAdding}
+}
