7271ca99167847d2cee75622f17a74b70020560b
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-11-07 01:04:15 +0000

    add react query for effective fetching and updating data

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,25 @@ import Dashboard from "./pages/Dashboard"
 import Courses from "./pages/Courses"
 import Reports from "./pages/Reports"
 import Course from "./pages/Course"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast"
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    }
+  }
+})
 
 function App() {
 
   return (
     <>
+    <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <BrowserRouter>
     <Routes>
       <Route
@@ -35,6 +48,27 @@ function App() {
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   </BrowserRouter>
+  <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "#18212f",
+              color: "#e5e7eb",
+            },
+          }}
+        />
+  </QueryClientProvider>
     </>
   )
 }
